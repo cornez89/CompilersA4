3ccb9dc54937d57e143958729d0c23f233b5bf49
benderje26 <jbjava00@gmail.com>
2025-04-24 00:55:34 -0500

    more errors fixed in semanticerrors1

--- src/semant/ClassEnvVisitor.java
+++ src/semant/ClassEnvVisitor.java
@@ -12,56 +12,26 @@ import util.ErrorHandler;
 
 public class ClassEnvVisitor extends SemantVisitor {
 
-    // get all parent fields and put them in current class symbol table
-    // ClassTreeNode parent = classTreeNode.getParent();
-    // while(parent != null)
-    // {
-    // Class_ parentAST = parent.getASTNode();
-
-    // // get an iterator of the parents member list
-    // Iterator<ASTNode> iterator = parentAST.
-    // getMemberList().getIterator();
-
-    // // check each member and see if it's a field, if it
-    // // is, add it to the current classTreeNode symbol table
-    // while(iterator.hasNext())
-    // {
-    // ASTNode currentMember = iterator.next();
-
-    // if(currentMember instanceof Field)
-    // {
-    // Field field = (Field) currentMember;
-    // classTreeNode.getVarSymbolTable().add
-    // (field.getName(), field.getType());
-    // }
-    // }
-    // }
     protected boolean methodArgsMatch(Method method1, Method method2) {
         boolean isValid = true;
         if (method1.getFormalList().getSize() != method2.getFormalList().getSize()) {
-            errorHandler.register(errorHandler.SEMANT_ERROR,
-                    classTreeNode.getASTNode().getFilename(),
-                    0,
-                    "number of actual parameters (" + method1.getFormalList().getSize()
-                            + ") differs from number of formal parameters ("
-                            + method2.getFormalList().getSize()
-                            + ") in dispatch to method '" + method1.getName() + "'");
+            registerSemanticError(method1,
+                    "overriding method '" + method1.getName() + "' has " + method1.getFormalList().getSize()
+                            + " formals, which differs from the inherited method (" + method2.getFormalList().getSize()
+                            + ")");
             isValid = false;
         } else {
             Iterator<ASTNode> formals1 = method1.getFormalList().getIterator();
             Iterator<ASTNode> formals2 = method2.getFormalList().getIterator();
-            int i = 0;
+            int i = 1;
             while (formals1.hasNext()) {
                 String type1 = ((Formal) formals1.next()).getType();
                 String type2 = ((Formal) formals2.next()).getType();
                 if (!type1.equals(type2)) {
-                    errorHandler.register(errorHandler.SEMANT_ERROR,
-                            classTreeNode.getASTNode().getFilename(),
-                            0, "actual parameter " + i + " with type '" +
-                                    type1 + "'' does not match formal parameter "
-                                    + i + " with declared type '" +
-                                    type2 + "' in dispatch to method'"
-                                    + method1.getName() + "'");
+                    registerSemanticError(method1,
+                            "overriding method '" + method1.getName()
+                                    + "' has formal type '" + type1 + "' for formal " + i
+                                    + ", which differs from the inherited method's formal type '" + type2 + "'");
                     isValid = false;
                 }
                 i++;
@@ -71,8 +41,13 @@ public class ClassEnvVisitor extends SemantVisitor {
     }
 
     protected boolean isValidMethodOverride(Method method) {
-        Method originalMethod = (Method) classTreeNode.getMethodSymbolTable().lookup(method.getName());
+        Method originalMethod = (Method) classTreeNode.getParent().getMethodSymbolTable().lookup(method.getName());
         boolean isValid = true;
+
+        if (methodArgsMatch(method, originalMethod)) {
+            // already registered errors
+            isValid = false;
+        }
 
         if (!method.getReturnType().equals(originalMethod.getReturnType())) {
             errorHandler.register(errorHandler.SEMANT_ERROR,
@@ -81,11 +56,6 @@ public class ClassEnvVisitor extends SemantVisitor {
                     "overriding method '" + method.getName() + "' has return type '" + method.getReturnType()
                             + "', which differs from the inherited method's return type '"
                             + originalMethod.getReturnType() + "'");
-            isValid = false;
-        }
-
-        if (methodArgsMatch(method, originalMethod)) {
-            // already registered errors
             isValid = false;
         }
 
@@ -151,16 +121,16 @@ public class ClassEnvVisitor extends SemantVisitor {
     }
 
     public Object visit(Method node) {
-        if (classTreeNode.getMethodSymbolTable().peek(node.getName()) != null) {
-
-            // name already exists
-            registerSemanticError(node, "method '" + node.getName() +
-                    "' is already defined in class '" + classTreeNode.getName() + "'");
-        } else if (classTreeNode.getMethodSymbolTable().lookup(node.getName()) != null) {
+        if (classTreeNode.getParent() != null
+                && classTreeNode.getParent().getMethodSymbolTable().lookup(node.getName()) != null) {
 
             // method will report errors for us
             isValidMethodOverride(node);
             classTreeNode.getMethodSymbolTable().add(node.getName(), node);
+        } else if (classTreeNode.getMethodSymbolTable().peek(node.getName()) != null) {
+            // name already exists
+            registerSemanticError(node, "method '" + node.getName() +
+                    "' is already defined in class '" + classTreeNode.getName() + "'");
         } else {
             classTreeNode.getMethodSymbolTable().add(node.getName(), node);
         }
