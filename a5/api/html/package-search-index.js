394feb7ef1fc472c62eee8565fd0ff0a27fad075
Zach Cornell <cornez89@uwosh.edu>
2025-05-01 15:41:21 -0500

    ammend

--- a5/api/html/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ast"},{"l":"codegenjvm"},{"l":"parser"},{"l":"semant"},{"l":"util"},{"l":"visitor"}];updateSearchResults();
